aaf89f49dd60c6e863d84ec671b1978dd02a6110
Unknown <vladymyr.martynov@gmail.com>
2017-06-19 03:47:41 +0300

    Last step
    At last! Menu!

--- app/libs/menu/static-menu.js
+++ app/libs/menu/static-menu.js
@@ -1,29 +1,54 @@
-// JavaScript Document
-
-
-jQuery(function($){
-	// появление/затухание кнопки #back-top
-	$(function (){
-		// прячем кнопку #back-top
-		$("#menu-top");
-	
-		$(window).scroll(function (){
-			if ($(this).scrollTop() > 600){
-				$("#menu-top").addClass('static-menu animated fadeInDown');
-				$(".alert-callback").css("display","block");
-				
-			} else{
-				$("#menu-top").removeClass('static-menu animated fadeInDown');
-				$(".alert-callback").css("display","none");
-			}
-		});
-
-		
-	
-	});
-
-
-});
-
-
-
+// JavaScript Document
+
+
+
+
+
+jQuery(function($){
+
+	// появление/затухание кнопки #back-top
+
+	$(function (){
+
+		// прячем кнопку #back-top
+
+		$("#head");
+
+	
+
+		$(window).scroll(function (){
+
+			if ($(this).scrollTop() > 600){
+
+				console.log("red");
+
+				
+
+			} else{
+
+				$("#head").removeClass('static-menu');
+
+			}
+
+		});
+
+
+
+		
+
+	
+
+	});
+
+
+
+
+
+});
+
+
+
+
+
+
+
